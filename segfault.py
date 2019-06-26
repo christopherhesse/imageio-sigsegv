7e608a98860924b4e34d0e5ceb0ab4ad22b673aa
Christopher Hesse <christopherhesse@users.noreply.github.com>
2019-06-26 10:57:41 -0700

    Update segfault.py

--- segfault.py
+++ segfault.py
@@ -1,24 +1,3 @@
-# import imageio
-# import concurrent.futures
-# from glob import glob
-# import sys
-# import os
-
-
-# def make_iterator(uris):
-#     while True:
-#         for uri in uris:
-#             r = imageio.get_reader(uri=uri, format="ffmpeg")
-#             yield r.get_next_data()
-
-
-# def main():
-#     uris = glob(os.path.join(sys.argv[1], "*/*.mp4"))
-#     its = [make_iterator(uris) for _ in range(9)]
-#     with concurrent.futures.ThreadPoolExecutor(max_workers=len(its)) as executor:
-#         for i in range(100000):
-#             print(i, len(list(executor.map(next, its))))
-    
 import imageio
 import queue
 import threading
